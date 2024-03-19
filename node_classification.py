f813995b53b7f7001096f22b3a4eefc1f2236dc3
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2024-03-19 10:29:26 -0400

    debugging

--- node_classification.py
+++ node_classification.py
@@ -76,7 +76,13 @@ class GAT(torch.nn.Module):
 
 class Experiment:
     def __init__(self, dataset, num_layers, num_heads=1):
+        # remove self-loops and convert to undirected graph
         dataset.data.edge_index = to_undirected(dataset.data.edge_index)
+        G = to_networkx(dataset.data)
+        self_loops = list(nx.selfloop_edges(G))
+        G.remove_edges_from(self_loops)
+        dataset.data = from_networkx(G)
+
         self.dataset = dataset
         self.num_layers = num_layers
         self.num_heads = num_heads
