9bd7fd5a59f0e5ae53aa33744aee8aa5604b1556
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2024-04-13 20:42:24 -0400

    added visualization methods

--- graph_classification.py
+++ graph_classification.py
@@ -233,6 +233,52 @@ class Experiment:
             G.add_edge(i, i, weight=att)
         return G, attention_scores
 
+
+class VisualizationMethods:
+    """
+    An abstract class with static
+    methods for visualizing attention graphs.
+    """
+
+    @staticmethod
+    def visualize_attention_digraph(G):
+        """
+        Visualize the attention scores of the given directed graph
+        with edges with low attention scores in blue and edges with
+        high attention scores in red. Set the max attention score
+        to 1 and the min attention score to 0.
+        """
+        edge_colors = [G[u][v]["weight"] for u, v in G.edges]
+        edge_colors = [(1 - w, 0, w) for w in edge_colors]
+        # add a legend for the edge colors to the plot
+        fig, ax = plt.subplots()
+        sm = plt.cm.ScalarMappable(cmap=plt.cm.Reds, norm=plt.Normalize(vmin=0, vmax=1))
+        sm.set_array([])
+        fig.colorbar(sm, label="Attention Score")
+        nx.draw(G, with_labels=False, node_color="lightgrey", node_size=50, edge_color=edge_colors, width=2.0, edge_cmap=plt.cm.Reds)
+        plt.show()
+
+    @staticmethod
+    def visualize_attention_differences(G):
+        """
+        Create a new graph where for each pair of undirected edges (u, v) and (v, u),
+        we add a new undirected edge with weight equal to the absolute difference
+        between the attention scores of the original edges. Visualize the new graph
+        with edges with low attention differences in blue and edges with high attention
+        differences in red. Set the max attention difference to 1 and the min attention
+        difference to 0.
+        """
+        H = nx.Graph()
+        for u, v in G.edges:
+            if u != v:
+                w1 = G[u][v]["weight"]
+                w2 = G[v][u]["weight"]
+                H.add_edge(u, v, weight=abs(w1 - w2))
+        edge_colors = [H[u][v]["weight"] for u, v in H.edges]
+        edge_colors = [(1 - w, 0, w) for w in edge_colors]
+        nx.draw(H, with_labels=True, node_color="lightgrey", node_size=50, edge_color=edge_colors, width=2.0, edge_cmap=plt.cm.Reds)
+        plt.show()
+
     
 class AttentionDeviation:
     """
